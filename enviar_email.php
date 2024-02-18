58bd029a27f46df1fccbadf883abc4cbc7ad13e2
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-02-18 17:32:46 -0300

    fazendo_atualizacoes_no_portifolio

---
+++ enviar_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $assunto = $_POST['assunto'];
+    $mensagem = $_POST['mensagem'];
+
+    $to = "l.henrick@live.com";
+    $subject = $assunto;
+    $message = "Nome: $nome\n";
+    $message .= "Email: $email\n\n";
+    $message .= "Mensagem:\n$mensagem\n";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email enviado com sucesso!";
+    } else {
+        echo "Erro ao enviar o email.";
+    }
+}
+?>
